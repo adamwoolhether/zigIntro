9bdee005174387af6f3e801312672da2ca1b179f
adamwoolhether <adamwoolhether@gmail.com>
2023-05-01 20:16:17 +0800

    stucts

--- 01-basics.zig
+++ 01-basics.zig
@@ -524,3 +524,74 @@ test "hmm" {
     Mode.count += 1;
     try expect(Mode.count == 1);
 }
+
+// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// STRUCTS
+// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// The most common kind of composit data type in Zig, allow defining types that can store
+// a fixed set of named fields. There is no guarantee about the in-memory order of struct-
+// fields or its size.
+// Syntax: T{}
+
+// Declaring and filling a struct:
+const Vec3 = struct {
+    x: f32,
+    y: f32,
+    z: f32,
+};
+
+test "struct usage" {
+    const my_vector = Vec3{
+        .x = 0,
+        .y = 100,
+        .z = 50,
+    };
+    _ = my_vector;
+}
+
+// All fields must be given a value. This test will fail.
+// test "missing struct field" {
+//     const my_vector = Vec3{
+//         .x = 0,
+//         .z = 50,
+//     };
+//     _ = my_vector;
+// }
+
+// Fields can be given defaults
+const Vec4 = struct {
+    x: f32,
+    y: f32,
+    z: f32 = 0,
+    w: f32 = undefined,
+};
+
+test "struct defaults" {
+    const my_vector = Vec4{
+        .x = 25,
+        .y = -50,
+    };
+    _ = my_vector;
+}
+
+// Struct can contain func declarations.
+// When given a pointer to a struct, one level of dereferencing is done automatically
+// when accessing the fields. Notice in this example: `self.x` and `self.y` are accessed
+// in the swap function without needing to derefernce the self pointer.
+const Stuff = struct {
+    x: i32,
+    y: i32,
+    fn swap(self: *Stuff) void {
+        const tmp = self.x;
+        self.x = self.y;
+        self.y = tmp;
+    }
+};
+
+test "automatic dereference" {
+    var thing = Stuff{ .x = 10, .y = 20};
+    thing.swap();
+    try expect(thing.x == 20);
+    try expect(thing.y == 10);
+}
