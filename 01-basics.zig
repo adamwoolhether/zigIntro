c943cb34c9a22e9baa569d4a2ef6e9cf541c8eac
adamwoolhether <adamwoolhether@gmail.com>
2023-05-16 21:00:42 +0800

    labelled loops

--- 01-basics.zig
+++ 01-basics.zig
@@ -723,3 +723,28 @@ test "int-float conversion" {
     const c = @floatToInt(i32, b);
     try expect(c == a);
 }
+
+// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// LABELLED BLOCKS
+// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Blocks are expressions that can be given a label, used to yield values, meaning they can be used in place of a value.
+// Here we use a label called `blk`. An empty block's value is the type `void`.
+test "labelled blocks" {
+    const count = blk: {
+        var sum: u32 = 0;
+        var i: u32 = 0;
+        while (i < 10) : (i += 1) sum += i;
+        break :blk sum;
+    };
+    try expect(count == 45);
+    try expect(@TypeOf(count) == u32);
+}
+// This is the equivalent of C's i++
+// blk: {
+//     const tmp = i;
+//     i += 1;
+//     break :blk tmp;
+// }
+
+
