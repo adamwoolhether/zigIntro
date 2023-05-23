f6d34ea01e8ef3cd2ba8c9e21d44e0f32fdf97b8
adamwoolhether <adamwoolhether@gmail.com>
2023-05-23 21:34:54 +0800

    comptime

--- 01-basics.zig
+++ 01-basics.zig
@@ -764,7 +764,7 @@ test "while loop expression" {
 // /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Syntax: `?T`, optionals are used to store the data `null` or a value of type `T`.
-// TODO: WHY DOESN'T THIS WORK
+// ************************************************************** BROKEN
 // test "optional" {
 //     var found_index: ?usize = null;
 //     const data = [_]i32{ 1, 2, 3, 4, 5, 6, 7, 8, 12 };
@@ -831,3 +831,162 @@ test "while null capture" {
 // they use the 0 value of the pointer for null.
 // Null pointers in Zig must be unwrapped to a non-optional before dereferencing.
 // This stops null pointer dereferences from happening accidentally.
+
+// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// COMPTIME
+// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Blocks of code can be forcibly executed at compile time with `comptime`.
+// `x` and `y` are equivalent in this example.
+
+test "comptime blocks" {
+    var x = comptime fibonacci(10);
+    _ = x;
+
+    var y = comptime blk: {
+        break :blk fibonacci(10);
+    };
+
+    _ = y;
+}
+
+// Integer literals are of `comptime_int` type. They're special in that they have no size(can't be used at runtime)
+// and have arbitrary precision. `comptime_int` values coerce to any integer type that can hold them.
+// They also coerce to floats. Character literals are also of this type.
+test "comptime_int" {
+    const a = 12;
+    const b = a + 10;
+
+    const c: u4 = a;
+    _ = c;
+    const u: f32 = b;
+    _ = u;
+}
+
+// `comptime_float`, internally an f123, is also available.
+// They can't be coerced to integers, even if they hold a value.
+// Type in Zig are values of the type `type`. They're available at compiule time.
+test "branching on types" {
+    const a = 5;
+    const b: if (a < 10) f32 else i32 = 5;
+    _ = b;
+}
+
+// Func parameters in Zig can be tagged as a `comptime`. The value passed to that function parameter must be known
+// at compile time.
+// Here we make a function that returns a type. Notice the func is PascalCase, as it returns a type.
+fn Matrix(
+    comptime T: type,
+    comptime width: comptime_int,
+    comptime height: comptime_int,
+) type {
+    return [height][width]T;
+}
+
+test "returning a type" {
+    try expect(Matrix(f32, 4, 4) == [4][4]f32);
+}
+
+// We can reflect upon types with builtin `@typeInfo`, which returns a type and returns a tagged
+// union. The tagged union can be found in `std.builtin.TypeInfo`.
+// ************************************************************** BROKEN
+// fn addSmallInts(comptime T: type, a: T, b: T) T {
+//     return switch (@typeInfo(T)) {
+//         .ComptimeInt => a + b,
+//         .Int => |info| if (info.bits <= 16)
+//             a + b
+//         else
+//             @compileError("ints too large"),
+//         else => @compileError("only ints accepted"),
+//     };
+// }
+//
+// test "typeinfo switch" {
+//     const x = addSmallInts(u16, 20, 30);
+//     try expect(@TypeOf(x) == 16);
+//     try expect(x == 50);
+// }
+
+// a `@Type` func can create a type from `@typeInfo`. `@Type` is implemented for most types,
+// but notably unimplemented for enums, unions, functions, and structs.
+// This example uses an anonymous struct syntax with `.{}`, because `T` in `T{}` can be inferred.
+// This example will give us a compile error if the `Int` tag isn't set.
+fn GetBiggerInt(comptime T: type) type {
+    return @Type(.{
+        .Int = .{
+            .bits = @typeInfo(T).Int.bits + 1,
+            .signedness = @typeInfo(T).Int.signedness,
+        },
+    });
+}
+
+test "@Type" {
+    try expect(GetBiggerInt(u8) == u9);
+    try expect(GetBiggerInt(i31) == i32);
+}
+
+// Returning a struct type is how to make generic data structs in Zig.
+// `@This` is required here, which gets the type of the inntermost struct,
+// union, or enum. `std.mem.eql` is used here to compare two slices.
+// ************************************************************** BROKEN
+// fn Vec(
+//     comptime count: comptime_int,
+//     comptime T: type,
+// ) type {
+//     return struct {
+//         data: [count]T,
+//         const Self = @This();
+//
+//         fn abs(self: Self) Self {
+//             var tmp = Self{ .data = undefined };
+//             for (self.data, 0..) |elem, i| {
+//                 tmp.data[i] = if (elem < 0)
+//                     -elem
+//                 else
+//                     elem;
+//             }
+//             return tmp;
+//         }
+//
+//         fn init(data: [count]T) Self {
+//             return Self{ .data = data };
+//         }
+//     };
+// }
+//
+//
+const eql = @import("std").mem.eql;
+//
+// test "generic vector" {
+//     const x = Vec(3, f32).init([_]f32{ 10, -10, 5});
+//     const y = x.abs();
+//     try expect(eql(f32, &y.data, &[_]f32{ 10, 10, 5 }));
+// }
+
+// Function parameter types can also be inferred by using `anytime` in place of `type`. `@TypeOf` can then be used.
+fn plusOne(x: anytype) @TypeOf(x) {
+    return x + 1;
+}
+
+test "inferred function parameter" {
+    try expect(plusOne(@as(u32, 1)) == 2);
+}
+
+// Comptime also introduces the `++` and `**` operators for concatenation and repeating arrays/slices.
+// These operators don't work at runtime.
+test "++" {
+    const x: [4]u8 = undefined;
+    const y = x[0..];
+
+    const a: [6]u8 = undefined;
+    const b = a[0..];
+
+    const new = y ++ b;
+    try expect(new.len == 10);
+}
+
+test "**" {
+    const pattern = [_]u8{ 0xCC, 0xAA };
+    const memory = pattern ** 3;
+    try expect(eql(u8, &memory, &[_]u8{ 0xCC, 0xAA, 0xCC, 0xAA, 0xCC, 0xAA }));
+}
